235d22047630e3e8e8951e9b5dbbb2743c523f84
Nina1823 <lilianarivera310@gmail.com>
2023-01-15 19:31:56 -0500

    Puertox2

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@
 require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const puerto = process.env.PUERTO || 3001
+const port = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(puerto, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log('%s listening at '+port); // eslint-disable-line no-console
   });
 });
